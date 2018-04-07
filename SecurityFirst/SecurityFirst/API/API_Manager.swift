0b1018420ea202ecd2b44518070741840a68d83d
Luiz Henrique <lhs2@cin.ufpe.br>
2018-04-07 10:08:04 -0300

    Criação do .gitignore do projeto ignorando arquivos não necessários de Swift e CocoaPods no repositório.

---
+++ SecurityFirst/SecurityFirst/API/API_Manager.swift
@@ -0,0 +1,9 @@
+//
+//  API_Manager.swift
+//  SecurityFirst
+//
+//  Created by de Sousa, Luiz H. on 06/04/2018.
+//  Copyright © 2018 de Sousa, Luiz H. All rights reserved.
+//
+
+import Foundation
